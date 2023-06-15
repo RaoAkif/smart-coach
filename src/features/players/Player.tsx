ba798e0cc45d03cd1e0398957ddb84ae4866b868
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-15 19:28:10 +0500

    Add All Players Design to the Dashboard.

--- src/features/players/Player.tsx
+++ src/features/players/Player.tsx
@@ -1,26 +1,55 @@
 import React from "react";
 
 interface PlayerProps {
-  player: {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    number: string;
-    position: string;
-  };
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  number: string;
+  position: string;
 }
 
 const Player: React.FC<PlayerProps> = ({ player }) => {
   return (
-    <div>
-      <h3>{player.name}</h3>
-      <p>Username: {player.username}</p>
-      <p>Email: {player.email}</p>
-      <p>Number: {player.number}</p>
-      <p>Position: {player.position}</p>
-      {/* Render other player details as needed */}
-    </div>
+    <tr className="p-4 border-b-2 border-gray-300 bg-white">
+      <td className="w-1/6 text-left pl-4 h-16">{player.name}</td>
+      <td className="w-1/6 text-left pl-4 h-16">{player.position}</td>
+      <td className="w-1/6 text-left pl-4 h-16">{player.email}</td>
+      <td className="w-1/6 text-left pl-4 h-16">{player.number}</td>
+      <td className="w-1/6 text-left pl-4 h-16">
+        <button className="flex mt-2 bg-white border border-solid border-blue-700 text-blue-700 rounded-md justify-center items-center gap-1 px-2 py-1" style={{ width: '86px', height: '36px' }}>
+          <img
+            src="/assets/icons/EyeIcon.png"
+            alt="Options Icon"
+            width={17}
+            height={17}
+          />
+          <span className="text-sm">View</span>
+        </button>
+      </td>
+      <td>
+        {/* Edit button */}
+        <button className="text-blue-700">
+          <img
+            src="/assets/icons/EditIcon.png"
+            alt="Options Icon"
+            width={17}
+            height={17}
+          />
+        </button>
+      </td>
+      <td>
+        {/* Delete button */}
+        <button className="text-blue-700">
+          <img
+            src="/assets/icons/DeleteIcon.png"
+            alt="Options Icon"
+            width={17}
+            height={17}
+          />
+        </button>
+      </td>
+    </tr>
   );
 };
 
--- src/features/players/Players.tsx
+++ src/features/players/Players.tsx
@@ -22,10 +22,46 @@ const Players = () => {
   }
 
   return (
-    <div>
-      {players?.map((player: PlayerProps) => (
-        <Player key={player.id} player={player} />
-      ))}
+    <div className="bg-white rounded-xl border border-solid border-gray-300 w-full m-10">
+      <div>
+        <div className="flex justify-between items-center p-4 border-b-2 border-solid border-gray-300">
+          <div className="flex justify-between items-center p-4">
+            <div className="flex items-center gap-4">
+              <h2 className="text-lg font-bold">All Players</h2>
+              <div className="flex items-center bg-blue-100 text-blue-700 font-semibold justify-center border-2 border-blue-300 rounded-full px-2 py-1" style={{ width: '74px', height: '22px', fontSize: '12px' }}>
+                <span>{players.length}</span>
+                <span className="ml-1">
+                  {players.length === 1 ? 'Player' : 'Players'}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 ml-auto">
+            <button className="flex items-center justify-center bg-blue-700 text-white px-2 py-1 rounded-md" style={{ width: '133px', height: '40px' }}>
+              <img src="/assets/icons/WhiteAddIcon.png" alt="Add Icon" className="w-4 h-4 mr-1" />
+              <span>Add Player</span>
+            </button>
+          </div>
+        </div>
+
+        <table className="bg-gray-100 w-full">
+          <thead className="h-11 border-b-2 border-gray-300">
+            <tr>
+              <th className="w-1/6 text-left pl-4">Name</th>
+              <th className="w-1/6 text-left pl-4">Position</th>
+              <th className="w-1/6 text-left pl-4">Email</th>
+              <th className="w-1/6 text-left pl-4">Mobile</th>
+              <th className="w-1/6 text-left pl-4">Login Details</th>
+              <th className="w-1/12" colSpan={2}></th> {/* Empty space for edit and delete columns */}
+            </tr>
+          </thead>
+          <tbody>
+            {players?.map((player: PlayerProps) => (
+              <Player key={player.id} player={player} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
