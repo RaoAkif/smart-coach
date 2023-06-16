92c15d599aeab34a3e3503c2f822f956f1ef9822
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-16 22:04:54 +0500

    Update apiSlice.ts

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://smart-coach-api.vercel.app/api',
     prepareHeaders: (headers) => {
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDb2FjaEluZm8iOnsiY29hY2hJZCI6MX0sImlhdCI6MTY4NjgxMTM3OCwiZXhwIjoxNjg3NDE2MTc4fQ.8erhupn7NVfkJtwzd26P87ZfopyA1jtsgviCSrbTjrw';
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDb2FjaEluZm8iOnsiY29hY2hJZCI6MX0sImlhdCI6MTY4NjkzNDg4NywiZXhwIjoxNjg3NTM5Njg3fQ.nqxeMCZMbyCsqEn3chFfFYTZfohERTCTEfbZO6KLmb8';
       headers.set('Authorization', `Bearer ${token}`);
       return headers;
     },
