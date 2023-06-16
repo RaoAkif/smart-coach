3262348854201be4d2e28e87a761eda3893f5276
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-16 11:02:47 +0500

    Update DashLayout.tsx

--- src/components/dashComponents/DashLayout.tsx
+++ src/components/dashComponents/DashLayout.tsx
@@ -1,5 +1,5 @@
-import Menubar from './DashHeader';
-import Sidebar from './DashSidebar';
+import DashHeader from './DashHeader';
+import DashSidebar from './DashSidebar';
 import { Outlet } from 'react-router-dom';
 
 const DashLayout = () => {
@@ -7,10 +7,10 @@ const DashLayout = () => {
     <div className="max-w-[1450px] mx-auto">
       <div className="flex">
         <aside className="w-2/12 pt-5">
-          <Sidebar />
+          <DashSidebar />
         </aside>
         <main className="border-l-2 border-[#eaecf0] w-full">
-          <Menubar />
+          <DashHeader />
           <Outlet />
         </main>
       </div>
