16b75c403e855faf3ec06e6460d4253e0a5896df
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-16 21:21:21 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import reactJsx from 'vite-react-jsx';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'https://smart-coach-api.vercel.app/api', // Replace with your backend API URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
