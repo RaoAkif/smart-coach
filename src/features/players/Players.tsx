182c6a715bf369d4b6806dfbb13daf23aa994024
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-16 12:43:52 +0500

    Add Create Player Model

--- src/features/players/Players.tsx
+++ src/features/players/Players.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useGetPlayersQuery } from "./playersApiSlice";
 import Player from "./Player";
 
@@ -12,6 +13,15 @@ interface PlayerProps {
 
 const Players = () => {
   const { data: players, isLoading, error } = useGetPlayersQuery({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -37,7 +47,7 @@ const Players = () => {
             </div>
           </div>
           <div className="flex items-center gap-2 ml-auto">
-            <button className="flex items-center justify-center bg-blue-700 text-white px-2 py-1 rounded-md" style={{ width: '133px', height: '40px' }}>
+            <button className="flex items-center justify-center bg-blue-700 text-white px-2 py-1 rounded-md" style={{ width: '133px', height: '40px' }} onClick={openModal}>
               <img src="/assets/icons/WhiteAddIcon.png" alt="Add Icon" className="w-4 h-4 mr-1" />
               <span>Add Player</span>
             </button>
@@ -74,6 +84,55 @@ const Players = () => {
           </tbody>
         </table>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg p-6 overflow-y-auto" style={{ width: '544px', height: '480px' }}>
+            <h2 className="text-xl font-bold mb-6 text-center">Add Player Details</h2>
+            <form className="flex flex-col gap-4">
+              <label htmlFor="name">Full Name</label>
+              <input type="text" id="name" name="name" className="border border-gray-300 rounded-md px-2 py-1 w-full" placeholder="Adrian Dubler" />
+
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" className="border border-gray-300 rounded-md px-2 py-1 w-full" placeholder="@adrian" />
+
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" className="border border-gray-300 rounded-md px-2 py-1 w-full" placeholder="adrian@gmail.com" />
+
+              <label htmlFor="number">Phone Number</label>
+              <input type="text" id="number" name="number" className="border border-gray-300 rounded-md px-2 py-1 w-full" placeholder="+1 234 567 890" />
+
+              <label htmlFor="position">Position</label>
+              <select id="position" name="position" className="border border-gray-300 rounded-md px-2 py-1 w-full">
+                <option value="">Select a position</option>
+                <option value="GOAL_KEEPER">GOAL_KEEPER</option>
+                <option value="FULL_BACK_RIGHT">FULL_BACK_RIGHT</option>
+                <option value="FULL_BACK_LEFT">FULL_BACK_LEFT</option>
+                <option value="CENTRE_BACK">CENTRE_BACK</option>
+                <option value="SWEEPER">SWEEPER</option>
+                <option value="DEFENSIVE_MIDFIELD_RIGHT">DEFENSIVE_MIDFIELD_RIGHT</option>
+                <option value="DEFENSIVE_MIDFIELD_LEFT">DEFENSIVE_MIDFIELD_LEFT</option>
+                <option value="SECOND_STRIKER">SECOND_STRIKER</option>
+                <option value="CENTRE_FORWARD">CENTRE_FORWARD</option>
+              </select>
+
+              <label htmlFor="availability">Availability Status</label>
+              <select id="availability" name="availability" className="border border-gray-300 rounded-md px-2 py-1 w-full">
+                <option value="PENDING">PENDING</option>
+                <option value="GOING">GOING</option>
+                <option value="NOT_GOING">NOT GOING</option>
+              </select>
+
+              <div className="flex justify-between">
+                <button className="bg-white font-semibold text-gray-700 border border-gray-300 px-4 py-2 rounded-md w-5/12">Cancel</button>
+                <button className="bg-blue-700 text-white px-4 py-2 rounded-md w-5/12">Add Player</button>
+              </div>
+            </form>
+
+            {/* Modal content */}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
