3bb1dcac0a8de6288ec1d73fd8f007addb2106c2
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-20 16:04:52 +0500

    Correct the bottom border and add icons

--- src/features/teams/Teams.tsx
+++ src/features/teams/Teams.tsx
@@ -79,65 +79,90 @@ const Teams = () => {
               <h2 className="text-lg font-bold">All Teams</h2>
               <div className="flex items-center bg-blue-100 text-blue-700 font-semibold justify-center border-2 border-blue-300 rounded-full px-2 py-1" style={{ width: "74px", height: "22px", fontSize: "12px" }}>
                 <span>{teams.length}</span>
-                <span className="ml-1">{teams.length === 1 ? "Team" : "Teams"}</span>
+                <span className="ml-1">
+                  {teams.length === 1 ? "Team" : "Teams"}
+                </span>
               </div>
             </div>
           </div>
+          <div className="flex items-center gap-2 ml-auto">
+            <button className="flex items-center justify-center bg-blue-700 text-white px-2 py-1 rounded-md" style={{ width: '133px', height: '40px' }} onClick={() => openModal()}>
+              <img src="/assets/icons/WhiteAddIcon.png" alt="Add Icon" className="w-4 h-4 mr-1" />
+              <span>Add Team</span>
+            </button>
+          </div>
         </div>
-        <div className="p-4">
-          {teams.map((team) => (
-            <div key={team.id} className="flex items-center justify-between border-b-2 border-solid border-gray-300 p-4">
-              <div>
-                <h3 className="text-lg font-bold">{team.name}</h3>
-                <p>
-                  Players: {team.players.map((player) => player.name).join(", ")}
-                </p>
-              </div>
-              <div className="flex items-center gap-2">
-                <Link to={`/teams/${team.id}`} className="text-blue-700 underline">
-                  View
-                </Link>
-                <button className="text-red-700 underline" onClick={() => deleteTeamById(team.id)} disabled={isDeletingTeam}>
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))}
+        
+        <table className="w-full">
+  <thead className="h-11 border-b-2 bg-gray-100">
+    <tr>
+      <th className="w-1/6 text-left pl-4">Team Name</th>
+      <th className="w-3/6 text-left pl-4">Players</th>
+      <th className="w-1/6 text-left"></th>
+      <th className="w-1/6 text-left"></th>
+    </tr>
+  </thead>
+  <tbody>
+    {teams.length === 0 ? (
+      <tr>
+        <td colSpan={4}>
+          <button onClick={() => openModal()} className="flex text-center pt-5 pb-2 pl-5 justify-center items-center">
+            <img
+              src="/assets/icons/AddButtonDottedOutline.png"
+              alt="Add Icon"
+              className="mr-2"
+              width={25}
+              height={25}
+            />
+            <p className="text-sm bg-white">Add team</p>
+          </button>
+        </td>
+      </tr>
+    ) : (
+      teams.map((team: TeamProps) => (
+        <tr key={team.id} className="h-16 border-t border-gray-300 bg-white">
+          <td className="w-2/12 text-left pl-4">{team.name}</td>
+          <td className="w-7/12 text-left pl-4">
+            {team.players.map((player, index) => (
+              <span key={player.id}>
+                {player.name}
+                {index !== team.players.length - 1 && ", "}
+              </span>
+            ))}
+          </td>
+          <td className="text-right">
+            <button
+              onClick={() => openModal(team)}
+            >
+              <img
+                src="/assets/icons/EditIcon.png"
+                alt="Edit Icon"
+                width={17}
+                height={17}
+              />
+            </button>
+          </td>
+          <td className="text-right pr-5">
+            <button
+              onClick={() => deleteTeamById(team.id)}
+              disabled={isDeletingTeam}
+            >
+              <img
+                src="/assets/icons/DeleteIcon.png"
+                alt="Delete Icon"
+                width={17}
+                height={17}
+              />
+            </button>
+          </td>
+        </tr>
+      ))
+    )}
+  </tbody>
+</table>
+
         </div>
       </div>
-      {isModalOpen && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white rounded-lg p-4">
-            <h3 className="text-lg font-bold mb-2">{isAddingTeam ? "Add Team" : "Edit Team"}</h3>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold mb-1">Name:</label>
-              <input type="text" className="border border-solid border-gray-300 px-3 py-2 rounded-md w-full" value={teamDetails.name} onChange={(e) => setTeamDetails({ ...teamDetails, name: e.target.value })} />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold mb-1">Players:</label>
-              <input type="text" className="border border-solid border-gray-300 px-3 py-2 rounded-md w-full" value={teamDetails.players.map((player) => player.name).join(", ")} onChange={(e) => setTeamDetails({ ...teamDetails, players: e.target.value.split(", ") })} />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold mb-1">Event:</label>
-              <input
-                type="text"
-                className="border border-solid border-gray-300 px-3 py-2 rounded-md w-full"
-                value={teamDetails.event.title}
-                onChange={(e) => setTeamDetails({ ...teamDetails, event: { ...teamDetails.event, title: e.target.value } })}
-              />
-            </div>
-            <div className="flex justify-end">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md mr-2" onClick={saveTeam}>
-                Save
-              </button>
-              <button className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold px-4 py-2 rounded-md" onClick={closeModal}>
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
   );
 };
 
