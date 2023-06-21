f60ab0e952ba8d003f5ef30d0d593b77ecdd2327
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-21 18:25:30 +0500

    Add Register Coach Form

--- src/components/authComponents/RegisterForm.tsx
+++ src/components/authComponents/RegisterForm.tsx
@@ -1,6 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import { useAddCoachMutation } from "../../features/coaches/coachesApiSlice";
 import Logo from "../Logo";
 
+interface FormValues {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
 const RegisterForm = () => {
+  const navigate = useNavigate();
+  const [addCoach] = useAddCoachMutation();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.currentTarget);
+
+    // Get form values
+    const formValues: FormValues = {
+      name: formData.get("name") as string,
+      username: formData.get("username") as string,
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    };
+
+    try {
+      // Call the addCoach mutation
+      const response = await addCoach(formValues);
+
+      // Check if the form submission was successful
+      if ("error" in response) {
+        // Handle the error here if needed
+      } else {
+        // Form submission successful, navigate to /login route
+        navigate("/login");
+      }
+    } catch (error) {
+      // Handle any other errors here
+    }
+  };
+
   return (
     <div className="flex flex-col items-center mt-52">
       <Logo />
@@ -12,13 +53,19 @@ const RegisterForm = () => {
         Welcome coach! Please fill in your details.
       </p>
 
-      <form className="mt-8 w-64 lg:w-96">
+      <form className="mt-8 w-64 lg:w-96" onSubmit={handleSubmit}>
         <div className="relative border border-solid border-gray-300 rounded-xl p-2">
           <div className="h-1/2 border-gray-300"></div>
           <input
             name="name"
             type="text"
             placeholder="Enter your name"
+            className="w-full px-4 py-2 outline-none border-b border-solid border-gray-300"
+          />
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
             className="w-full px-4 py-2 outline-none border-b border-solid border-gray-300"
           />
           <input
