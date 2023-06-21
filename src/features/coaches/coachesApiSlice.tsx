6e9c453a4e50196530e3e5ff225c782f36a99fac
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-21 18:25:03 +0500

    Create coachesApiSlice.tsx

---
+++ src/features/coaches/coachesApiSlice.tsx
@@ -0,0 +1,49 @@
+import { apiSlice } from "../../redux/api/apiSlice";
+
+export const coachesApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getCoaches: builder.query({
+      query: (params) => ({
+        url: '/coaches',
+        method: 'GET',
+        params,
+      }),
+    }),
+    addCoach: builder.mutation({
+      query: (coach) => ({
+        url: '/auth/coach/login',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: coach,
+      }),
+    }),
+    editCoach: builder.mutation({
+      query: ({ id, ...coach }) => ({
+        url: `/coaches/${id}`,
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: coach,
+      }),
+    }),
+    deleteCoach: builder.mutation({
+      query: (id) => ({
+        url: `/coaches/${id}`,
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetCoachesQuery,
+  useAddCoachMutation,
+  useEditCoachMutation,
+  useDeleteCoachMutation
+} = coachesApiSlice;
