03f9d7b7ec2d94b490ff25ab6a9d2bf873f77755
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 11:14:40 +0500

    Get data in EventsDetails Page

--- src/features/events/EventDetails.tsx
+++ src/features/events/EventDetails.tsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useGetEventsWithPlayersQuery } from "./eventsApiSlice";
 
+type PlayerAvailabilityTypes = "PENDING" | "GOING" | "NOT_GOING";
+
 interface Player {
   id: number;
   name: string;
+  availability_status: PlayerAvailabilityTypes
 }
 
 const EventDetails = () => {
@@ -33,11 +36,18 @@ const EventDetails = () => {
       <p>End Time: {event.end_time}</p>
       <p>Location: {event.location}</p>
       <p>Details: {event.details}</p>
-      <p>Team: {event.Team.name}</p>
+      <p>Team: {event.team.name}</p>
       <p>Players:</p>
       <ul>
-        {event.Team.players.map((player: Player) => (
-          <li key={player.id}>{player.name}</li>
+        {event.team.players.map((player: Player) => (
+          <li key={player.id}>
+            <div>
+              {player.name}
+            </div>
+            <div>
+              {player.availability_status}
+            </div>
+          </li>
         ))}
       </ul>
     </div>
